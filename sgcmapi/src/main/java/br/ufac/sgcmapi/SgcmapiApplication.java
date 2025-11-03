c55a4944f97675721862b2f199be6587224d8715
Daniel Silva <danielnsilva@gmail.com>
2025-11-03 14:45:25 -0500

    feat: adiciona endpoint de teste

--- sgcmapi/src/main/java/br/ufac/sgcmapi/SgcmapiApplication.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/SgcmapiApplication.java
@@ -2,12 +2,20 @@ package br.ufac.sgcmapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SgcmapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgcmapiApplication.class, args);
+	}
+
+	@GetMapping(value = "/")
+	public String teste() {
+		return "SGCM";
 	}
 
 }
